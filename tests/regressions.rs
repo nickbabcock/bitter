c4b7d9ebaa2c501e8c427d62a4f12e37cda0109f
Nick Babcock <nbabcock19@hotmail.com>
2019-01-28 17:34:14 -0600

    Add a regression test

---
+++ tests/regressions.rs
@@ -0,0 +1,14 @@
+extern crate bitter;
+extern crate bitterv1;
+
+#[test]
+fn regression1() {
+    let data = vec![0b0000_0010, 0b0011_1111, 0b1011_1100];
+    let mut bits = bitter::BitGet::new(data.as_slice());
+    let mut bitsv1 = bitterv1::BitGet::new(data.as_slice());
+
+    assert_eq!(bits.read_u32_bits(3), bitsv1.read_u32_bits(3));
+    assert_eq!(bits.read_u8(), bitsv1.read_u8());
+    assert_eq!(bits.read_bit(), bitsv1.read_bit());
+    assert_eq!(bits.read_u32_bits(13), bitsv1.read_u32_bits(13));
+}
