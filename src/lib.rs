9030577e38dd49e97e95a54fa4438cf6e904b7a2
Nick Babcock <nbabcock19@hotmail.com>
2022-03-04 06:59:35 -0600

    Shorten read conditional

--- src/lib.rs
+++ src/lib.rs
@@ -946,8 +946,7 @@ impl<'a> LittleEndianReader<'a> {
             let res = (self.current_val >> self.pos) & bit_mask(bts);
             self.pos = new_pos;
             Some(res)
-        } else if self.end_pos == BIT_WIDTH {
-            let new_data = &self.data[BYTE_WIDTH..];
+        } else if let Some(new_data) = self.data.get(BYTE_WIDTH..) {
             let left = new_pos - BIT_WIDTH;
             if new_data.len() < BYTE_WIDTH && left > new_data.len() * 8 {
                 None
@@ -973,9 +972,8 @@ impl<'a> LittleEndianReader<'a> {
             self.data = &self.data[BYTE_WIDTH..];
             self.current_val = self.read();
             Some(ret)
-        } else if self.end_pos == BIT_WIDTH {
+        } else if let Some(new_data) = self.data.get(BYTE_WIDTH..) {
             let bts = core::mem::size_of::<u64>() * 8;
-            let new_data = &self.data[BYTE_WIDTH..];
             if new_data.len() < BYTE_WIDTH && self.pos > new_data.len() * 8 {
                 None
             } else {
